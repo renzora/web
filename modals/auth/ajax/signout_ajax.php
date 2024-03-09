91da9a3633789743b2b84427e0f73bee9040c466
renzora <106055871+renzora@users.noreply.github.com>
2024-03-09 07:51:39 +0000

    fixed bug where null is deprecated in setcookie

--- modals/auth/ajax/signout_ajax.php
+++ modals/auth/ajax/signout_ajax.php
@@ -4,6 +4,6 @@ include $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 if($auth) {
   if(isset($_COOKIE['renaccount'])) {
     unset($_COOKIE['renaccount']);
-    setcookie('renaccount', null, -1, '/');
+    setcookie('renaccount', '', time() - 3600, '/');
   }
 }
