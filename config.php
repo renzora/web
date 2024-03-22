f17f6a29068d77061612ce477eba96860a60a00f
James Grainger <88819475+pianoplayerjames@users.noreply.github.com>
2024-03-22 13:39:50 +0000

    updated php-jwt to 6.10
    also fixed a bug where use Firebase\JWT\KEY is no longer needed

--- config.php
+++ config.php
@@ -2,27 +2,25 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 use Firebase\JWT\JWT;
-use Firebase\JWT\KEY;
 
 try {
-
-  $db = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_DATABASE'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
-
+    $db = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_DATABASE'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
 } catch(PDOException $e) {
     print "Error: " . $e->getMessage();
     die();
 }
 
 if (!isset($_COOKIE['renaccount'])) {
-  $auth = FALSE;
-} else {
-  try {
-    $decoded = JWT::decode($_COOKIE['renaccount'], new Key($_ENV['JWT_KEY'], 'HS256'));
-    $user = $decoded;
-    $auth = TRUE;
-  } catch (Exception $e) {
-    setcookie('renaccount', null, -1, '/');
     $auth = FALSE;
-  }
+} else {
+    try {
+        // Adjusted to the correct usage for firebase/php-jwt v6.10.0
+        $decoded = JWT::decode($_COOKIE['renaccount'], new \Firebase\JWT\Key($_ENV['JWT_KEY'], 'HS256'));
+        $user = $decoded;
+        $auth = TRUE;
+    } catch (Exception $e) {
+        setcookie('renaccount', null, -1, '/');
+        $auth = FALSE;
+    }
 }
 ?>
